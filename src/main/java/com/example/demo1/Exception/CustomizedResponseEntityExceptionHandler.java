d2134cd70ae85deee84f8cd37f5e530c52dc1bb7
steveleejavaassembly <60907560+steveleejavaassembly@users.noreply.github.com>
2020-05-18 01:20:56 +0900

    [9] @Valid 조건 미부합시 Exception 컨트롤(AOP)

--- src/main/java/com/example/demo1/Exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/example/demo1/Exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,12 +1,16 @@
 package com.example.demo1.Exception;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import sun.misc.Request;
 
 import java.util.Date;
 
@@ -29,6 +33,15 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
                 new ExceptionResponse(new Date(),e.getMessage(),request.getDescription(false));
         //에러로부터 메세지받고, 클라이언트로 부터 받은 데이터 description은 없음
         return new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);//클라이언트 의 잘못이므로 400번 코드를 준다.
+    }
+    /*@Valid의 조건에 맞지 않는 매개변수가 들어올경우*/
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status,
+                                                                  WebRequest request){
+        ExceptionResponse exceptionResponse =new ExceptionResponse(new Date(),ex.getMessage(),ex.getBindingResult().toString());
+        return new ResponseEntity(exceptionResponse,HttpStatus.BAD_REQUEST);
     }
 
 }
