7c4e52e36200efd44c863241c93e01229c553d3b
steveleejavaassembly <60907560+steveleejavaassembly@users.noreply.github.com>
2020-05-17 21:41:42 +0900

    [3]PathVariable 으로 값 받기 (url에 노출)

--- src/main/java/com/example/demo1/HelloWorldController.java
+++ src/main/java/com/example/demo1/HelloWorldController.java
@@ -1,6 +1,7 @@
 package com.example.demo1;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,10 @@ public class HelloWorldController {
         //spring 프레임워크는 JSON 형태로 변환해서 반환해준다.
         //Restcontroller는 @ResponseBody를 이용하지 않아도  객체를 직접 반환시켰을때 , JSON형태로 자동 변환해서 반환해준다.
          return new HelloWorldBean("Hello world");
+    }
+    //같은 이름의 함수여도, 매개 변수가 다르다.
+    @GetMapping(path="/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldBean(@PathVariable String name){
+        return new HelloWorldBean(String.format("path-variable : %s",name));
     }
 }
