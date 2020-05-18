d0f3ab8418cd8f6e95c7b99d060712202233ecab
steveleejavaassembly <60907560+steveleejavaassembly@users.noreply.github.com>
2020-05-18 12:36:20 +0900

    [14-2] swagger설정 / HATAOAS 와 충돌 잡음

--- src/main/java/com/example/demo1/config/SwaggerConfig.java
+++ src/main/java/com/example/demo1/config/SwaggerConfig.java
@@ -1,5 +1,12 @@
 package com.example.demo1.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.hateoas.client.LinkDiscoverer;
+import org.springframework.hateoas.client.LinkDiscoverers;
+import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
+import org.springframework.plugin.core.SimplePluginRegistry;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.spi.DocumentationType;
@@ -15,4 +22,11 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2);
     }
 
+    @Bean
+    public LinkDiscoverers discoverers() {
+        List<LinkDiscoverer> plugins = new ArrayList<>();
+        plugins.add(new CollectionJsonLinkDiscoverer());
+        return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+
+    }
 }
