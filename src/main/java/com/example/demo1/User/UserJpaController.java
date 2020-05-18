ec2d8e0feb177b0d58064c7b3b813f0b8380ac29
steveleejavaassembly <60907560+steveleejavaassembly@users.noreply.github.com>
2020-05-19 00:32:28 +0900

    [16-5] findById & HATEOAS / by JPA

--- src/main/java/com/example/demo1/User/UserJpaController.java
+++ src/main/java/com/example/demo1/User/UserJpaController.java
@@ -1,10 +1,17 @@
 package com.example.demo1.User;
 
+import com.example.demo1.Exception.UserNotFoundException;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/jpa")
@@ -18,6 +25,21 @@ public class UserJpaController {
     public List<User> findAllUser(){
 
         return userRepository.findAll();
+
+    }
+    @GetMapping("/user/{id}")
+    public EntityModel<User> findById(@PathVariable int id){
+
+        Optional<User> user= userRepository.findById(id);
+        if(!user.isPresent()){
+            throw new UserNotFoundException(String.format("ID[%s] not found",id));
+        }
+        //HATEOAS
+        EntityModel<User> model = new EntityModel<>(user.get());
+        //전체 유저 찾는  uri 를 반환해준다
+        WebMvcLinkBuilder linkTo =WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).findAllUser());
+        model.add(linkTo.withRel("all-users"));
+        return model;
 
     }
 
