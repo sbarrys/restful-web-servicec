988b0c2475b9e8b89ed575e6da16c94ba6d9d834
steveleejavaassembly <60907560+steveleejavaassembly@users.noreply.github.com>
2020-05-18 10:08:30 +0900

    [11-2] @JsonFilter , Controller단에서 필터링 설정하기.

--- src/main/java/com/example/demo1/User/UserController_Admin.java
+++ src/main/java/com/example/demo1/User/UserController_Admin.java
@@ -1,7 +1,11 @@
 package com.example.demo1.User;
 
 import com.example.demo1.Exception.UserNotFoundException;
+import com.fasterxml.jackson.databind.ser.FilterProvider;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -19,19 +23,33 @@ public class UserController_Admin {
     }
 
     @GetMapping(path="/user")
-    public List<User> findAllUser(){
-        return userDao.findAll();
+    public MappingJacksonValue findAllUser(){
+        List<User> users= userDao.findAll();
+        //필터링 만들기.
+        SimpleBeanPropertyFilter filter= SimpleBeanPropertyFilter.filterOutAllExcept("id","name","joinDate","password","ssn");
+        MappingJacksonValue mapping=new MappingJacksonValue(users);
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserDomainClass",filter);
+        mapping.setFilters(filters);
+
+        return mapping;
     }
 
 
     @GetMapping(path="/user/{id}") //PathVariable 로 들어온 string 형태의 id가 int 형태로 자동 변환되서 들어간다.
-    public User findById(@PathVariable int id){
+    public MappingJacksonValue findById(@PathVariable int id){
+
         User user = userDao.findById(id);
         if(user==null){
             //RuntimeException을 상속받은 클래스를 해서 던져주었다.
             throw new UserNotFoundException(String.format("ID[%s] not Founded",id));
         }
-        return user;
+        //필터링 만들기.
+        SimpleBeanPropertyFilter filter= SimpleBeanPropertyFilter.filterOutAllExcept("id","name","joinDate","password","ssn");
+        MappingJacksonValue mapping=new MappingJacksonValue(user);
+        FilterProvider filters = new SimpleFilterProvider().addFilter("UserDomainClass",filter);
+        mapping.setFilters(filters);
+
+        return mapping;
     }
 
 }
