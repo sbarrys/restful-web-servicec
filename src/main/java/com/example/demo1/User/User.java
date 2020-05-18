8472b3fec604004f5abf3179041e9939f94c445b
steveleejavaassembly <60907560+steveleejavaassembly@users.noreply.github.com>
2020-05-18 23:51:34 +0900

    [16-3] JPA 초기Data 셋팅 (data.sql)

--- src/main/java/com/example/demo1/User/User.java
+++ src/main/java/com/example/demo1/User/User.java
@@ -3,10 +3,14 @@ package com.example.demo1.User;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.Past;
@@ -16,10 +20,13 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel(description = "User-Detail")
+@Entity
 //@JsonIgnoreProperties(value={"password","ssn"})
 //@JsonFilter("UserDomainClass")
 public class User {
-
+    @Id
+    @GeneratedValue//이를 기반으로 엔티티테이블 생성, 자동으로 생성되는 값
     private Integer id;
     @Size(min=2)
     private String name;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into user values(1,sysdate(),'User1','password1','80800-1011');
+insert into user values(2,sysdate(),'User2','password2','80800-1022');
+insert into user values(3,sysdate(),'User3','password3','80800-1033');
